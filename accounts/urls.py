a609ec881ede56c82868d3e780f729b4ce3945e2
henry-stewart <hfstewart23@gmail.com>
2025-01-30 14:49:49 -0500

    henry test commit. added stuff to readme file

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('signup', views.signup, name='accounts.signup'),
+    path('login/', views.login, name='accounts.login'),
+    path('logout/', views.logout, name='accounts.logout'),
 ]
