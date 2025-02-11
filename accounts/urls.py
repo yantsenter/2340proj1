974910b7e15fae34f5d7f240daffbc0aae093ac7
dayda <davisr12344@gmail.com>
2025-02-11 15:34:02 -0500

    Davis

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 urlpatterns = [
     path('signup', views.signup, name='accounts.signup'),
     path('login/', views.login, name='accounts.login'),
     path('logout/', views.logout, name='accounts.logout'),
     path('orders/', views.orders, name='accounts.orders'),
+    path('reset/', views.reset_password, name='accounts.reset'),
+
 ]
