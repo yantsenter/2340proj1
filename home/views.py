680f1895489374af0f9177f74ffad19ecfc5c84c
kobef <kobefowler05@gmail.com>
2025-02-13 15:23:18 -0500

    Added to welcome page

--- home/views.py
+++ home/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 
+from movies.models import Movie
+
+
 def index(request):
+    search_term = request.GET.get('search')
+    if search_term:
+        movies = Movie.objects.filter(name__icontains=search_term)
+    else:
+        movies = Movie.objects.all()
+
     template_data = {}
-    template_data['title'] = 'Movies Store'
+    template_data['title'] = 'Movies'
+    template_data['movies'] = movies
     return render(request, 'home/index.html', {'template_data': template_data})
 
 def about(request):
